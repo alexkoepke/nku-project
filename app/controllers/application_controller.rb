4c8fb56af3abe282b8acb11ad7ebd1673cb7addc
Alexander <alex.koepke@gmail.com>
2014-04-18 18:51:44 +0000

    fix/make pretty update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,14 @@ class ApplicationController < ActionController::Base
   
 
   add_flash_types :success, :info, :warning, :danger
+
+  protected
+ 
+  def devise_parameter_sanitizer
+    if resource_class == User
+      User::ParameterSanitizer.new(User, :user, params)
+    else
+      super
+    end
+  end
 end
